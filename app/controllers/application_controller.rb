6720c685f065c0b35c02eac6a3a01ea28e63211e
Tom Counsell <tom@counsell.org>
2011-10-18 22:18:39 +0100

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      return true if user_name.downcase == "decc" && password.downcase == "benefit"
+      return true if user_name.downcase == "delib" && password.downcase == "limestonebattery"
+      return false
+    end
+  end
+  
 end
