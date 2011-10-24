235989bafa75c59435b556723ea605612b60aa38
Tom Counsell <tom@counsell.org>
2011-10-24 16:45:12 +0100

    Doesn't reconnect to the mongodb every request

--- app/controllers/pathways_controller.rb
+++ app/controllers/pathways_controller.rb
@@ -1,15 +1,23 @@
 class PathwaysController < ApplicationController
   
+  cattr_accessor :connection, :db
+  
   def data
-    conn = Mongo::Connection.from_uri(ENV['MONGO_URI'])
-    db   = conn.db(ENV['MONGO_DB'])
-    p = db.collection('pathways').find_one(_id:params[:id])
+    p = database.collection('pathways').find_one(_id:params[:id])
     if p
       expires_in 1.minute, :public => true
     else
-      db.collection('pathways_to_be_calculated').insert({_id:params[:id]})
+      database.collection('pathways_to_be_calculated').insert({_id:params[:id]})
     end
     render :json => p
+  end
+  
+  def database
+    unless PathwaysController.db
+      PathwaysController.connection = Mongo::Connection.from_uri(ENV['MONGO_URI'])
+      PathwaysController.db   = PathwaysController.connection.db(ENV['MONGO_DB'])
+    end
+    PathwaysController.db
   end
   
 end
