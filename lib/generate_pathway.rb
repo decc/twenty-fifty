84b988d7aebf08ce9bf7cf93b2175fd9d6a1c6ff
Tom Counsell <tom@counsell.org>
2011-10-24 14:56:08 +0100

    Added more data to the pathway object

--- lib/generate_pathway.rb
+++ lib/generate_pathway.rb
@@ -10,11 +10,13 @@ class GeneratePathway
   
   def calculate_pathway(code)
     @pathway = { _id: code, choices: set_choices(code) }
-    table :ghg, 182, 192
-    table :final_energy_demand, 13, 18
-    table :primary_energy_supply, 278, 291
-    pathway[:sankey] = intermediate_output_sheet.a('h365','j455').to_grid
+    primary_energy_tables
+    electricity_tables
+    sankey_table
+    energy_security_table
     cost_components_table
+    heating_choice_table
+    map_table
     pathway
   end
   
@@ -27,12 +29,69 @@ class GeneratePathway
     choices
   end
   
-  def table(name,start_row,end_row)
+  def primary_energy_tables
+    table pathway, :ghg, 182, 192
+    table pathway, :final_energy_demand, 13, 18
+    table pathway, :primary_energy_supply, 278, 291
+  end
+  
+  def electricity_tables
+    e = {}
+    table e, :demand, 318, 321
+    table e, :supply, 99, 111
+    table e, :emissions, 265, 268
+    table e, :capacity, 118, 132
+    e['automatically_built'] = intermediate_output_sheet.q120
+    pathway['electricity'] = e
+  end
+  
+  def sankey_table
+    pathway[:sankey] = intermediate_output_sheet.a('h365','j455').to_grid
+  end
+  
+  def heating_choice_table
+    h = {}
+    h['residential'] = r = {}
+    [ 'Gas boiler (old)',
+      'Gas boiler (new)',
+      'Resistive heating',
+      'Oil-fired boiler',
+      'Solid-fuel boiler',
+      'Stirling engine in home CHP',
+      'Fuel-cell in home CHP',
+      'Air-source heat pump',
+      'Ground-source heat pump',
+      'Geothermal heat',
+      'Community scale gas CHP',
+      'Community scale solid-fuel CHP',
+      'District heating from power stations'].each_with_index do |name,i| 
+        r[name] = heating_sheet.send("f#{427+i}")
+      end
+    h['commercial'] = c = {}
+    ['Gas boiler (old)',
+      'Gas boiler (new)',
+      'Resistive heating',
+      'Oil-fired boiler',
+      'Solid-fuel boiler',
+      'Stirling engine in home CHP',
+      'Fuel-cell in home CHP',
+      'Air-source heat pump',
+      'Ground-source heat pump',
+      'Geothermal heat',
+      'Community scale gas CHP',
+      'Community scale solid-fuel CHP',
+      'District heating from power stations'].each_with_index do |name,i| 
+        r[name] = heating_sheet.send("n#{438+i}")
+      end
+    pathway[:heating] = h
+  end
+  
+  def table(target, name,start_row,end_row)
     t = {}
     (start_row..end_row).each do |row|
       t[label(intermediate_output_sheet,row)] = annual_data(intermediate_output_sheet,row)
     end
-    pathway[name] = t
+    target[name] = t
   end
   
   def label(sheet,row)
@@ -59,6 +118,39 @@ class GeneratePathway
     pathway['cost_components'] = t
   end
   
+  def map_table
+    m = {}
+    m['wave'] = intermediate_output_sheet.q350
+    [328..334,338..341,345..346,354..359].each do |range|
+      range.to_a.each do |row|
+        m[intermediate_output_sheet.send("c#{row}")] = intermediate_output_sheet.send("q#{row}")
+      end
+    end
+    pathway['map'] = m
+  end
+  
+  def energy_security_table
+    s = {}
+    s[:percent_energy_imported] = intermediate_output_sheet.q293
+    s[:gw_backup_needed] = control_sheet.cb9
+    [
+      ["Coal",37,39],
+      ["Oil",41,43],
+      ["Gas",44,46],
+      ["Bioenergy",35,36],
+      ["Uranium",23,23],
+      ["Electricity",110,111],
+      ["Primary energy",292,291]
+    ].each do |vector|
+      imported = intermediate_output_sheet.send("q#{vector[1]}").to_f
+      imported = imported > 0 ? imported.round : 0
+      total = intermediate_output_sheet.send("q#{vector[2]}").to_f
+      proportion = total > 0 ? "#{((imported/total) * 100).round}%" : "0%"
+      s[vector[0]] = { quantity: imported, proportion: proportion }
+    end
+    pathway['energy_security'] = s
+  end
+    
   def control_sheet
     excel.sheet1
   end
