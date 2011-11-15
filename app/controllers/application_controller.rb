4acc8b4afdc6bf14718d4b0df3c5fbaa75d3c279
Tom Counsell <tom@counsell.org>
2011-11-15 15:34:56 +0000

    Added extra passwords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate
+  
+  ACCOUNTS = {
+    'decc' => 'benefit',
+    'dbenton' => 'apple192',
+    'markbrinkley' => 'biscuit536',
+    'anthony.iles' => 'yoghurt278',
+    'duncan.rimmer' => 'milk923',
+    'mike.childs' => 'toast701'
+  }
 
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
-      return true if user_name.downcase == "decc" && password.downcase == "benefit"
-      return true if user_name.downcase == "delib" && password.downcase == "limestonebattery"
-      return false
+      ACCOUNTS.has_key?(user_name.downcase) && ACCOUNTS[user_name.downcase] == password.downcase
     end
   end
   
