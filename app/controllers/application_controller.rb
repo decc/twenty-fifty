aa5e9ba912f9be35c161c727b9ec4c77a8998739
Tom Counsell <tom@counsell.org>
2011-12-01 10:58:23 +0000

    Removed password protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate
+  #before_filter :authenticate
   
   ACCOUNTS = {
-    'decc' => 'benefit',
-    'dbenton' => 'apple192',
-    'markbrinkley' => 'biscuit536',
-    'anthony.iles' => 'yoghurt278',
-    'duncan.rimmer' => 'milk923',
-    'mike.childs' => 'toast701'
   }
 
   def authenticate
