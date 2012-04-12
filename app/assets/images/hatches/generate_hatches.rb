9c4af33a9d8ca500d4bc426d7a17a72c39fff8ed
Tom Counsell <tom@counsell.org>
2012-04-12 19:20:07 +0100

    Moved the generate_hatches utility to lib/tasks from app/assets/images/hatches to avoid bug in precompiling assets

--- app/assets/images/hatches/generate_hatches.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-%w{f00 8c564b 2ca02c 1f77b4 ff7f0e d62728 7f7f7f EA8BCC a55194 fff928}.each do |color|
-  puts "Creating ##{color}-hatch.png"
-  puts `convert hatch-blackandwhite.png -fill '##{color}' -opaque '#000000' hatch-#{color}.png`
-end
---
+++ lib/tasks/generate_hatches.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+image_root = File.expand_path(File.join(File.dirname(__FILE__),'..','..','app','assets','images','hatches'))
+%w{f00 8c564b 2ca02c 1f77b4 ff7f0e d62728 7f7f7f EA8BCC a55194 fff928}.each do |color|
+  puts "Creating ##{color}-hatch.png in #{image_root}"
+  puts `convert #{image_root}/hatch-blackandwhite.png -fill '##{color}' -opaque '#000000' #{image_root}/hatch-#{color}.png`
+end
