8724c59955a48e1a544f4a7446d3c797ceb9f0e9
Tom Counsell <tom@counsell.org>
2012-06-04 19:26:26 +0100

    Make it possible not to leave off the 'primary_energy_chart' at the end of a url

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails3::Application.routes.draw do
   # New routes
   match ':controller/:id/costs_compared_within_sector/:sector(.:format)', :action => 'costs_compared_within_sector'
   match ':controller/:id/:action/comparator/:comparator(.:format)'
-  match ':controller/:id/:action(.:format)'
+  match ':controller/:id/(:action)(.:format)',  :defaults => { :action => 'primary_energy_chart' }
   
   # Home page and default pathways
   match '/' => redirect('/pathways/1111111111111111111111111111111111111111111111111111/primary_energy_chart')
