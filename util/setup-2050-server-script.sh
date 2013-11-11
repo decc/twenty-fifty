3269ecd1eea7eab28d5e5c3ee71229eaac1c4d71
Tom Counsell <tamc@ubuntu.(none)>
2013-11-11 00:08:24 +0000

    Improved the setup-2050-server-script

--- util/setup-2050-server-script.sh
+++ util/setup-2050-server-script.sh
@@ -1,48 +1,77 @@
-## REQUIRED LIBRARIES
+#!/bin/bash
+
+# This script sets up a Ubuntu 12.04 server to run the 2050 server
+# To use sh setup-2020-server-script.sh
+# You will be prompted for information at a couple of points
+
+# The first thing we need to do is check we are running the right version of Ubuntu
+. /etc/lsb-release # This loads variables which contain the Ubuntu version
+if [ "$DISTRIB_ID" != 'Ubuntu' ] || [ "$DISTRIB_RELEASE" != '12.04' ]; then
+  echo "This script is only tested with Ubuntu 12.04. (your system is reported as $DISTRIB_ID $DISTRIB_RELEASE)"
+  exit 1
+fi
+
+# First step is to get the system working in development mode
 
 # Basics
 sudo apt-get update
 sudo apt-get install -y git build-essential libxml2-dev libxslt-dev zip unzip
-sudo apt-get install -y libcurl4-openssl-dev libssl-dev apache2-mpm-prefork apache2-prefork-dev libapr1-dev libaprutil1-dev nodejs
+sudo apt-get install -y libcurl4-openssl-dev libssl-dev nodejs
 
-# Ruby, Apache and mod_rails (aka Passenger)
+# Ruby 2
 sudo apt-add-repository -y ppa:brightbox/ruby-ng-experimental
-sudo apt-add-repository -y ppa:brightbox/passenger
 sudo apt-get update 
 sudo apt-get upgrade -y
-sudo apt-get install -y ruby1.9.3
+sudo apt-get install -y ruby2.0 ruby2.0-dev
 
 ## THE CODE
-git clone git://github.com/decc/twenty-fifty.git
+read -ep "Enter the url of the github repository (just press ENTER to accept the default): " -i "https://github.com/decc/twenty-fifty" GITHUB
 
-# Bundler
+git clone $GITHUB
+cd $(basename $GITHUB)
+
+read -ep "Enter the branch of the repository you wish to install (just press ENTER to accept master, which is the latest, or backspace to delete master and type oldHighCharts to get a version compatible with older web browsers):" -i "master" GITBRANCH
+
+git checkout -b $GITBRANCH origin/$GITBRANCH
+
+# Bundler is a Ruby library that installs all the ruby language depdencies 
 sudo gem install --no-ri --no-rdoc bundler
-cd twenty-fifty
-bundle
-bundle exec rake assets:precompile  
+sudo bundle
 
-# THE SERVER
+echo "You can now run this server in development mode by typing rackup and then opening a web browser to http://0.0.0.0:9292"
+
+read -p "Would you like to set up the Apache web server to run this? Not needed for development, just when you want to run a live version for the world (Y/N)" -n 1 CONT
+echo
+if [[ ! "$CONT" =~ ^[Yy]$ ]]; then
+  exit 0 
+fi
+
+# This next bit is only required if you want to set the server up in production mode
+sudo apt-get install -y apache2-mpm-prefork apache2-prefork-dev libapr1-dev libaprutil1-dev
 
 sudo gem install --no-ri --no-rdoc passenger
 sudo passenger-install-apache2-module --auto
-sudo sh -c 'echo "LoadModule passenger_module /var/lib/gems/1.9.1/gems/passenger-4.0.23/buildout/apache2/mod_passenger.so" > /etc/apache2/mods-available/passenger.load'
-sudo sh -c 'echo "PassengerRoot /var/lib/gems/1.9.1/gems/passenger-4.0.23" > /etc/apache2/mods-available/passenger.conf'
-sudo sh -c 'echo "PassengerRuby /usr/bin/ruby1.9.1" >> /etc/apache2/mods-available/passenger.conf'
+
+
+passenger-install-apache2-module --snippet > /etc/apache2/mods-available/passenger.load 
+touch /etc/apache2/mods-available/passenger.conf
 sudo a2enmod passenger
 
-sudo sh -c 'echo "<VirtualHost *:80>" > /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "ServerName 2050-calculator-tool.decc.gov.uk" >> /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "DocumentRoot /home/iept/twenty-fifty/public" >> /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "<Directory /home/iept/twenty-fifty/public>" >> /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "  AllowOverride all" >> /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "  Options -MultiViews" >> /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "</Directory>" >> /etc/apache2/sites-available/twenty-fifty'
-sudo sh -c 'echo "</VirtualHost>" >> /etc/apache2/sites-available/twenty-fifty'
+read -ep "Enter the public url that people will use to reach this machine and access the calculator:" -i "2050-calculator-tool.decc.gov.uk" PUBLICURL
+
+cat > /etc/apache2/sites-available/twenty-fifty << EOF
+<VirtualHost *:80>
+ServerName $PUBLICURL
+DocumentRoot $PWD/public
+<Directory $PWD/public>
+  AllowOverride all
+  Options -MultiViews
+</Directory>
+</VirtualHost>
+EOF
+
 sudo a2ensite twenty-fifty
 
 sudo service apache2 restart
-
-
-
-
+echo "If $PUBLICURL is correctly configured to point at this machine, then should now be possible to access the 2050 calculator from there"
 
