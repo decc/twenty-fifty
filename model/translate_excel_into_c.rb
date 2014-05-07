6a2b9fe84c1e479b328c7d56e51b9ca5ff6a0090
Tom Counsell <tom@counsell.org>
2014-05-07 17:17:05 +0100

    Translation of excel into C now keeps all of the Control worksheet cells

--- model/translate_excel_into_c.rb
+++ model/translate_excel_into_c.rb
@@ -1,4 +1,6 @@
-unless require 'excel_to_code'
+begin
+  require 'excel_to_code'
+rescue LoadError
   puts "You need to install excel_to_code, try sudo gem install excel_to_code or possibly sudo gem21 install excel_to_code"
   exit
 end
@@ -12,25 +14,13 @@ def setup(command)
   command.cells_that_can_be_set_at_runtime = { "Control" => (5.upto(57).to_a.map { |r| "e#{r}" }) }
 
   command.cells_to_keep = {
-    # The names, limits, 10 worders, long descriptions
-    "Control" => (5.upto(57).to_a.map { |r| ["d#{r}","f#{r}","h#{r}","i#{r}","j#{r}","k#{r}","bo#{r}","bp#{r}","bq#{r}","br#{r}"] }).flatten, 
+    "Control" => :all,
     "Intermediate output" => :all, 
     "CostPerCapita" => :all, 
     "Land Use" => :all, 
     "Flows" => :all, 
     "AQ Outputs" => :all, 
   }
-  
-  # Add the version number
-  command.cells_to_keep["Control"] << "m1"
-  
-  # Add the example pathways
-  rows = (4..60).to_a
-  ('m'..'z').to_a.push('aa','ab').each do |column|
-    rows.each do |row|
-      command.cells_to_keep["Control"].push("#{column}#{row}")
-    end
-  end
 
   command.actually_compile_code = true
   command.actually_run_tests = true
