e737dead9258bf5c97ff60ad288be6efc554216b
Tom Counsell <tom@counsell.org>
2014-05-07 08:12:48 +0100

    Refactored out the separate model_choice model_alternative and model_utilities files

--- model/model_structure.rb
+++ model/model_structure.rb
@@ -1,7 +1,11 @@
 require_relative 'model'
 require 'singleton'
 
-class ModelStructure < ModelUtilities
+class ModelChoice
+  attr_accessor :number, :name, :type, :descriptions, :long_descriptions, :incremental_or_alternative, :levels, :doc
+end
+
+class ModelStructure
   include Singleton
   
   attr_accessor :excel, :choices
@@ -10,14 +14,68 @@ class ModelStructure < ModelUtilities
     @excel = ModelShim.new
     @choices = []
     types.each_with_index do |choice_type,i|
-      case choice_type
-      when nil, 0.0; next
-      when /[abcd]/i; choices << ModelAlternative.new(i,names[i],choice_type,descriptions[i],long_descriptions[i])
-      else; choices << ModelChoice.new(i,names[i],choice_type,descriptions[i],long_descriptions[i])
-      end
+      next if choice_type == nil
+      next if choice_type == 0.0
+      incremental = choice_type =~ /[abcd]/i
+      choice = ModelChoice.new
+      choice.number = i
+      choice.name = names[i]
+      choice.type = choice_type
+      choice.incremental_or_alternative =  incremental ? 'alternative' : 'incremental'
+      choice.descriptions = descriptions[i]
+      choice.long_descriptions = long_descriptions[i]
+      choice.levels = incremental ? 'A'.upto(choice_type.upcase) : 1.upto(choice_type.to_i)
+      choice.doc = FILE_NAMES_OF_ONE_PAGE_NOTES[i] || "#{i}.pdf"
+      choices << choice
     end
   end
   
+  FILE_NAMES_OF_ONE_PAGE_NOTES = {
+    0 => '0.pdf',
+    2 => '2.pdf',
+    3 => '3.pdf',
+    4 => '4.pdf',
+    5 => '5.pdf',
+    6 => '6.pdf',
+    7 => 'TidalStream.pdf',
+    8 => 'TidalRange.pdf',
+    9 =>  '7.pdf',
+    10 => '8.pdf',
+    11 => '9.pdf',
+    12 => '10.pdf',
+    13 => '11.pdf',
+    14 => '12.pdf',
+    15 => '13.pdf',
+    16 => '14.pdf',
+    17 => '15.pdf',
+    18 => '16.pdf',
+    19 => '17.pdf',
+    20 => '18.pdf',
+    21 => '19.pdf',
+    22 => '20.pdf',
+    25 => '23.pdf',
+    26 => '24.pdf',
+    27 => 'FuelCellsOrBatteries.pdf',
+    28 => '25.pdf',
+    29 => 'InternationalAviation.pdf',
+    30 => 'InternationalShipping.pdf',
+    32 => '29.pdf',
+    33 => '30.pdf',
+    34 => '31.pdf',
+    35 => '31.pdf',
+    37 => '34.pdf',
+    38 => '35.pdf',
+    40 => '37.pdf',
+    41 => '38.pdf',
+    43 => '40.pdf', 
+    44 => '31.pdf',
+    45 => '31.pdf',
+    47 => '44.pdf',
+    48 => '35.pdf',
+    50 => '47.pdf',
+    51 => '48.pdf' 
+  }
+
   def reported_calculator_version
     excel.control_m1
   end
@@ -97,10 +155,33 @@ class ModelStructure < ModelUtilities
 
   def choice_sizes
     sizes = {}
-    ModelStructure.instance.choices.each do |choice|
+    choices.each do |choice|
       sizes[choice.number] = choice.levels.to_a.size
     end
     sizes
   end
 
+  def r(reference)
+    excel.send(reference)
+  end
+
+  # Set the 9 decimal points between 1.1 and 3.9
+  FLOAT_TO_LETTER_MAP = Hash["abcdefghijklmnopqrstuvwxyzABCD".split('').map.with_index { |l,i| [(i/10.0)+1,l] }]
+  FLOAT_TO_LETTER_MAP[0.0] = '0'
+  FLOAT_TO_LETTER_MAP[1.0] = '1'
+  FLOAT_TO_LETTER_MAP[2.0] = '2'
+  FLOAT_TO_LETTER_MAP[3.0] = '3'
+  FLOAT_TO_LETTER_MAP[4.0] = '4'
+  
+  LETTER_TO_FLOAT_MAP = FLOAT_TO_LETTER_MAP.invert
+  
+  def convert_float_to_letters(array)
+    array.map do |entry|
+      case entry
+      when Float; FLOAT_TO_LETTER_MAP[entry] || entry
+      when nil; 0
+      else entry
+      end
+    end
+  end
 end
