4674ab27d32bc9102e00521b9de35a19b4ae548b
Tom Counsell <tom@counsell.org>
2014-05-07 08:35:25 +0100

    Refactored the controls from the helper into the erb file

--- src/server.rb
+++ src/server.rb
@@ -14,14 +14,6 @@ module Helper
   def assets=(h)
     @assets = h
   end
-  
-  def classic_table_row_for_choice(choice)
-    row = ["<td class='name'><a href='/assets/onepage/#{choice.doc}' target='_new' onmouseover='twentyfifty.startDemo(#{choice.number}); return true;' onmouseout='twentyfifty.stopDemo(#{choice.number});return true;'>#{choice.name}</a></td>", "<td class='help'><a title='Click for more detail on what choices #{choice.levels.to_a.join(' ')} mean.' href='/assets/onepage/#{choice.doc}' target='_new'>?</a></td>"]
-    choice.levels.each.with_index do |level,i|
-      row << "<td class='choice'><a href='#' data-choicenumber='#{choice.number}' data-choicelevel='#{i+1}' id='c#{choice.number}l#{i+1}' title='#{choice.descriptions[i]}' class='choiceLink' >#{level}</a></td>"
-    end
-    "<tr class='#{choice.incremental_or_alternative}' id='r#{choice.number}'>#{row.join('')}</tr>"
-  end
 
   def structure
     @structure ||= ModelStructure.new
