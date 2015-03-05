fd5fac674711a2725efb57ed429eeceaaeaf1ca1
Tom Counsell <tom@counsell.org>
2015-03-05 18:25:52 +0000

    Added some more examples of using an optimiser, this time to find pathways with lots of gas

---
+++ model/optimiser/most_gas_for_target_no_geosequestration_middle_industry.c
@@ -0,0 +1,30 @@
+#include "optimiser.c"
+
+static void setup_run() {
+  printf("Optimising for most natural gas in 2050 but also meeting the 80%% emissions reduction target, without geosequestration, without shrinking industry\n\n");
+  number_of_choices[40] = 2; // Limit Industry growth choice to level 1 or 2
+  number_of_choices[50] = 1; // Limit Geosequestration choice to level 1
+}
+
+// This is the key optimisation method, which defined what GOOD is.
+// Must return an int, with a higher number meaning more fit.
+static int calculate_fitness_of(Candidate candidate) {
+  int emissions_reduction, gas_in_2050;
+  reset();
+  set_input_choices(candidate.choices);
+  emissions_reduction = (int) (output_emissions_percentage_reduction().number * 100);
+  gas_in_2050 = (int) (flows_n21().number);
+  // If emissions reduction is less than 80, count that as fitness
+  if(emissions_reduction < 80) { return emissions_reduction; }
+  // Subtract cost per capita from 1 million so that positive numbers are fitter
+  return gas_in_2050;
+}
+
+// The fitness often won't be very meaningful, so when presenting results can be
+// usefull to show more data about the candidate
+static void describe(Candidate candidate) {
+  reset();
+  set_input_choices(candidate.choices);
+  printf("Reduction in emissions 1990-2050: %2.0f%% natural gas in 2050: %2.0f%%", output_emissions_percentage_reduction().number * 100.0, flows_n21().number);
+}
+
