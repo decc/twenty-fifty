11f57a3cceae05904b0a7cb8effc110d68f2d207
tamc <tamc@greenonblack.com>
2017-04-08 17:08:24 +0000

    Updated translation of Excel to C in response to commit 3739bb2feab3bbff87b1de7842f73fb86554a569 tinkered with the optimiser

--- model/model_version.rb
+++ model/model_version.rb
@@ -1,1 +1,1 @@
-def Model.last_modified_date() @last_modified_date ||= Time.utc(*[10, 24, 7, 10, 3, 2015, 2, 69, false, "GMT"]); end
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[44, 15, 16, 8, 4, 2017, 6, 98, false, "UTC"]); end
